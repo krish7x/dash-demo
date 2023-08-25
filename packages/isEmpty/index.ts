39056981234b901321d7d2f10a03193531f4d1db
krish7x <krishnakumar99g@gmail.com>
2023-08-25 18:01:58 +0530

    feat: changes

--- packages/isEmpty/index.ts
+++ packages/isEmpty/index.ts
@@ -1,15 +1,17 @@
 export default (value: any) => {
   if (value === null || value === undefined) {
-    return true;
+    return true
   }
 
-  if (typeof value === "string" || Array.isArray(value)) {
-    return value.length === 0;
+  if (typeof value === 'string' || Array.isArray(value)) {
+    return value.length === 0
   }
 
-  if (typeof value === "object") {
-    return Object.keys(value).length === 0;
+  if (typeof value === 'object') {
+    return Object.keys(value).length === 0
   }
 
-  return false;
-};
+
+
+  return false
+}
